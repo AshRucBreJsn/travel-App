71d1d8f3a43ddc97baeaa9943d33a29e6f31797e
ruchigoswami <awasthiruchi.us@gmail.com>
2017-09-04 20:26:21 -0400

    changed signup logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@
     var email =$("#email").val().trim();
     var name =  $("#name").val().trim(); 
     var password=$("#password").val().trim();
-
+    window.location.href = "../html/login.html";
      /* database.ref().push({
           name:  $("#name").val().trim(),
           email:  $("#email").val().trim(),
@@ -47,24 +47,25 @@
       var password=$("#loginPassword").val().trim();
       
 
-      /*database.ref().on("value", function(snapshot) {
-      
-      $("#form-employeeName").html(snapshot.val().name);
-      name = snapshot.val().name;
-      });*/
+        if(userId === null || userId.trim()==='' 
+              || password === null || password.trim()==='' ){
 
-      var userRef = firebase.database().ref('users/' + userId);
-      userRef.on('value', function(snapshot) {
-      if(snapshot.val() == null){
-       $("#passwordResult").text("User Not Found, please signup");
-      } else if(snapshot.val().password === password){
-          window.location.href = "../html/home.html";
-      }else{
-         $("#passwordResult").text("Password is wrong.");
-      }
-      
-      });
+           $("#passwordResult").text("Please enter valid value for all fields");
 
+      }else {
 
+          var userRef = firebase.database().ref('users/' + userId);
+          userRef.on('value', function(snapshot) {
+          if(snapshot.val() == null){
+           $("#passwordResult").text("User Not Found, please signup");
+          } else if(snapshot.val().password === password){
+              window.location.href = "../html/home.html";
+          }else{
+             $("#passwordResult").text("Password is wrong.");
+          }
+          
+          });
+
+    }
 
    });
