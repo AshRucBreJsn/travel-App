3c61f4e64260029e65096387c8b318aa7e027ca2
ruchigoswami <awasthiruchi.us@gmail.com>
2017-09-06 19:20:46 -0400

    added point of intrest

---
+++ assets/javascript/poi.js
@@ -0,0 +1,42 @@
+
+$("#find").on("click",function(){
+
+var place = $("#place").val();
+var queryURL = "https://api.sygictravelapi.com/1.0/en/places/list?query="+place+"&level=poi&limit=100"
+
+$.ajax({
+    method: "GET",
+    url: queryURL,
+    headers: {
+        "x-api-key":"N8heiy7BHB7WXPkzfIj6CalObVtTTWiUDwGMPW08"
+    }
+  })
+  .done(function( response ) {
+  	console.log(response);
+    var places = response.data.places;
+    $("#displayPoi").empty();
+    for (var i = 0; i < places.length; i++){
+     
+      if(places[i].thumbnail_url != null && places[i].thumbnail_url !==''
+        && places[i].perex != null && places[i].perex !=='' ){
+        var newDiv = $("<div>");
+        newDiv.addClass("new-display");
+
+
+        var image = $("<img>");
+        image.attr("src", places[i].thumbnail_url);
+        newDiv.append(image);
+
+        var descDiv = $("<div>");
+        descDiv.html(places[i].perex);
+        newDiv.append(descDiv);
+        // Append the new Gifs to the animal-view
+        $("#displayPoi").append(newDiv);
+
+      }
+      
+    }
+  });
+
+});
+
